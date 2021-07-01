137c46244b7718192aa74b9a1379c4fd2cb58c1e
Madjid Sadallah <madjid.sadallah@gmail.com>
2021-07-01 16:07:33 +0200

    Preparing the app

--- src/App.js
+++ src/App.js
@@ -1,16 +1,23 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.png";
+import "./App.css";
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <h1>LAD Creator</h1>
-        <p>
-          Work in progress...
-        </p>
-        
+        <img src={logo} className="App-logo" alt="logo" />
+        <div className="actionButtons">
+          <button type="button">Change Color</button>
+          <button type="button">Change Color</button>
+          <button type="button">Change Color</button>
+        </div>
       </header>
+      <main className="App-header">
+        <p>
+          I love beagles <em>so</em> much! Like, really, a lot. They’re adorable
+          and their ears are so, so snuggly soft!
+        </p>
+      </main>
     </div>
   );
 }
