141cc29734e13a415a86d26c0fcd9fbce88baef9
Madjid Sadallah <madjid.sadallah@gmail.com>
2021-07-01 10:14:39 +0200

    Update App.js

--- src/App.js
+++ src/App.js
@@ -5,18 +5,11 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+        <h1>LAD Creator</h1>
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Work in progress...
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        
       </header>
     </div>
   );
